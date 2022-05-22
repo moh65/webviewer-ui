1ed6b9b4b97f44e84fdd4d14d74bc75b4ca29e36
Jeff <jeff@jeffmiller.com.au>
2022-05-22 16:13:52 +1000

    Fix tab spacing and add class

--- src/components/TagDropDown/TagDropDown.js
+++ src/components/TagDropDown/TagDropDown.js
@@ -1,7 +1,7 @@
 //customization-new-file
-import React, { Component, useState, useEffect, useRef, forwardRef, useImperativeHandle } from 'react'
-import Select from 'react-select'
-import { SketchPicker } from 'react-color'
+import React, { Component, useState, useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
+import Select from 'react-select';
+import { SketchPicker } from 'react-color';
 import actions from 'actions';
 import selectors from 'selectors';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
@@ -11,226 +11,231 @@ import { hexToRgba2 } from 'helpers/color';
 import setToolStyles from 'helpers/setToolStyles';
 
 export default forwardRef(({ setDropDownChanged, setSelectedTags, selectedTags, creatable, placeholder }, ref) => {
-    let [
-        token,
-        defaultTag,
-        createTagUrl,
-        getTagsUrl,
-        tagOptionsState,
-        disabledElements,
-        defaultBaseUrlAddress
-    ] = useSelector(
-        state => [
-            selectors.getAuthToken(state),
-            selectors.getDefaultTag(state),
-            selectors.getCreateTagUrl(state),
-            selectors.getGetTagsUrl(state),
-            selectors.getTagOptions(state),
-            selectors.getDisabledElements(state),
-            selectors.getDefaultUrlBaseAddress(state)
-        ]
-    );
+  let [
+    token,
+    defaultTag,
+    createTagUrl,
+    getTagsUrl,
+    tagOptionsState,
+    disabledElements,
+    defaultBaseUrlAddress
+  ] = useSelector(
+    state => [
+      selectors.getAuthToken(state),
+      selectors.getDefaultTag(state),
+      selectors.getCreateTagUrl(state),
+      selectors.getGetTagsUrl(state),
+      selectors.getTagOptions(state),
+      selectors.getDisabledElements(state),
+      selectors.getDefaultUrlBaseAddress(state)
+    ]
+  );
 
-    createTagUrl = createTagUrl ? createTagUrl : `${defaultBaseUrlAddress}/api/bundle/jobtag/664`;
-    getTagsUrl = getTagsUrl ? getTagsUrl : `${defaultBaseUrlAddress}/api/bundle/jobtag/664`;
+  createTagUrl = createTagUrl ? createTagUrl : `${defaultBaseUrlAddress}/api/bundle/jobtag/664`;
+  getTagsUrl = getTagsUrl ? getTagsUrl : `${defaultBaseUrlAddress}/api/bundle/jobtag/664`;
 
-    const noTagOption = { value: 'no-tag', label: 'No tag' }
-    const createTagOption = { value: 'create-tag', label: 'create new tag' }
+  const noTagOption = { value: 'no-tag', label: 'No tag' };
+  const createTagOption = { value: 'create-tag', label: 'create new tag' };
 
-    const dropDownRef = useRef();
+  const dropDownRef = useRef();
 
-    const [tagOptions, setTagOptions] = useState([]);
-    const [showTagCreateForm, setShowTagCreateForm] = useState(false);
-    const [tagName, setTagName] = useState('');
-    const [tagColor, setTagColor] = useState('');
-    const [showElement, setShowElement] = useState(false);
-    const dispatch = useDispatch();
+  const [tagOptions, setTagOptions] = useState([]);
+  const [showTagCreateForm, setShowTagCreateForm] = useState(false);
+  const [tagName, setTagName] = useState('');
+  const [tagColor, setTagColor] = useState('');
+  const [showElement, setShowElement] = useState(false);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const disableElement = disabledElements['tag-drop-down'];
-        if (disableElement == undefined) {
-            setShowElement(true);
-            dispatch(actions.enableElement('tag-drop-down'));
-        } else if (disableElement.disabled === false) {
-            setShowElement(true);
-        } else if (disableElement.disabled === true) {
-            setShowElement(false);
-        }
-
-    }, [disabledElements]);
-
-    useImperativeHandle(ref, () => ({
-
-        clearSelect() {
-            dropDownRef.current.select.clearValue();
-        }
-
-    }));
-
-    const customStyles = {
-        option: (provided, state) => {
-            return ({
-                ...provided,
-                color: state.data.value.split('-')[1]
-            })
-        },
-        multiValue: (provided, state) => {
-            return { ...provided, backgroundColor: state.data.value.split('-')[1] };
-        },
-        multiValueLabel: (provided, state) => {
-            return { ...provided, backgroundColor: state.data.value.split('-')[1] };
-        },
-        singleValue: (provided, state) => {
-            if (state.data.value === 'no-tag')
-                return { ...provided };
-            return { ...provided, backgroundColor: state.data.value.split('-')[1] };
-        },
-        singleValueLabel: (provided, state) => {
-            if (state.data.value === 'no-tag')
-                return { ...provided };
-            return { ...provided, backgroundColor: state.data.value.split('-')[1] };
-        }
+  useEffect(() => {
+    const disableElement = disabledElements['tag-drop-down'];
+    if (disableElement == undefined) {
+      setShowElement(true);
+      dispatch(actions.enableElement('tag-drop-down'));
+    } else if (disableElement.disabled === false) {
+      setShowElement(true);
+    } else if (disableElement.disabled === true) {
+      setShowElement(false);
     }
 
-    const colorPickerHandler = (color, event) => {
-        setTagColor(color.hex);
-    };
+  }, [disabledElements]);
 
-    const setAndCheckCreatableDropDown = (options) => {
-        let newOptions = [];
-        if (creatable) {
-            newOptions = [noTagOption, ...options, createTagOption];
-            setTagOptions(newOptions);
-        } else
-            setTagOptions(options);
+  useImperativeHandle(ref, () => ({
+
+    clearSelect() {
+      dropDownRef.current.select.clearValue();
     }
 
-    useEffect(() => {
-        let options = [];
-        if (!tagOptionsState.loaded) {
-            fetch(getTagsUrl, {
-                method: "GET",
-                headers: { "Authorization": `Bearer ${token}` }
-            }).then(res => res.json())
-                .then(json => {
-                    options = json.map(m => ({
-                        label: m.TagName,
-                        value: `${m.JobTagId}-${m.TagColour}`
-                    }));
-                    dispatch(actions.setTagOptions({ loaded: true, options: options }));
-                });
-        }
-    }, [])
+  }));
 
-    useEffect(() => {
-        setAndCheckCreatableDropDown(tagOptionsState.options);
-    }, [tagOptionsState.options]);
-
-
-    const saveTag = () => {
-        let body = { TagName: tagName, TagColour: tagColor, JobId: 0, JobTagId: 0 };
-        fetch(getTagsUrl, {
-            method: "POST",
-            headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json;charset=UTF-8" },
-            body: JSON.stringify(body)
-        }).then(async res => {
-            if (res.status === 200) {
-                let id = await res.json();
-
-                let newTagOptionState = {
-                    loaded: tagOptionsState.loaded, options: [...tagOptionsState.options, {
-                        label: tagName,
-                        value: `${id}-${tagColor}`
-                    }]
-                };
-
-                dispatch(actions.setTagOptions(newTagOptionState));
-            }
-        }).then(r => setShowTagCreateForm(false));
+  const customStyles = {
+    option: (provided, state) => {
+      return ({
+        ...provided,
+        color: state.data.value.split('-')[1]
+      });
+    },
+    multiValue: (provided, state) => {
+      return { ...provided, backgroundColor: state.data.value.split('-')[1] };
+    },
+    multiValueLabel: (provided, state) => {
+      return { ...provided, backgroundColor: state.data.value.split('-')[1] };
+    },
+    singleValue: (provided, state) => {
+      if (state.data.value === 'no-tag') {
+        return { ...provided };
+      }
+      return { ...provided, backgroundColor: state.data.value.split('-')[1] };
+    },
+    singleValueLabel: (provided, state) => {
+      if (state.data.value === 'no-tag') {
+        return { ...provided };
+      }
+      return { ...provided, backgroundColor: state.data.value.split('-')[1] };
     }
+  };
 
-    return (
-        showElement && (
-            <div style={{ width: '200px' }}>
-                {
-                    !showTagCreateForm ? <Select
-                        onChange={(option, { action }) => {
-                            if (creatable) {
-                                if (action === 'select-option') {
-                                    if (option.value === 'create-tag') {
-                                        setShowTagCreateForm(true);
-                                        return;
-                                    }
-                                    if (option.value === 'no-tag') {
-                                        dispatch(actions.setDefaultTag({}));
-                                        return;
-                                    }
-                                    let toolNames = ['AnnotationCreateTextHighlight',
-                                        'AnnotationCreateEllipse',
-                                        'AnnotationCreateRectangle',
-                                        'AnnotationCreateLine',
-                                        'AnnotationCreateFreeHand',
-                                        'AnnotationCreateFreeHandHighlight',
-                                        'AnnotationCreatePolygon',
-                                        'AnnotationCreatePolygonCloud',
-                                        'AnnotationCreatePolyline',
-                                        'AnnotationCreateArrow',
-                                        'AnnotationCreateFreeText',
-                                        'AnnotationCreateTextUnderline',
-                                        'AnnotationCreateTextStrikeout',
-                                        'AnnotationCreateTextSquiggly',
-                                        'AnnotationCreateSticky'];
-                                    let color = new window.Annotations.Color(option.value.split('-')[1]);
-                                    toolNames.forEach(toolName => {
-                                        if (toolName === 'AnnotationCreateFreeText') {
-                                            setToolStyles(toolName, 'TextColor', color);
-                                        } else {
-                                            setToolStyles(toolName, 'StrokeColor', color);
-                                        }
-                                    });
+  const colorPickerHandler = (color, event) => {
+    setTagColor(color.hex);
+  };
 
-                                    dispatch(actions.setDefaultTag(option));
-                                }
-                            } else {
-                                if (setDropDownChanged)
-                                    setDropDownChanged(true);
-                                if (setSelectedTags)
-                                    setSelectedTags(option);
-                            }
-                        }
-                        }
-                        ref={dropDownRef}
-                        placeholder={placeholder}
-                        defaultValue={creatable ? noTagOption : (defaultTag && defaultTag.value ? defaultTag : selectedTags)}
-                        isSearchable
-                        isClearable={creatable ? false : true}
-                        options={tagOptions}
-                        styles={customStyles}
-                        closeMenuOnSelect={creatable ? true : false}
-                        isMulti={creatable ? false : true}
-                        noOptionsMessage={e => "no tags available"}
-                    /> :
-                        <Swipeable onSwipedUp={e => setShowTagCreateForm(false)} onSwipedDown={e => setShowTagCreateForm(false)} preventDefaultTouchmoveEvent>
-                            <div style={{ marginTop: '380px' }}>
+  const setAndCheckCreatableDropDown = options => {
+    let newOptions = [];
+    if (creatable) {
+      newOptions = [noTagOption, ...options, createTagOption];
+      setTagOptions(newOptions);
+    } else {
+      setTagOptions(options);
+    }
+  };
 
-                                <label htmlFor='tagName'>Tag Name:</label>
-                                <input type='text' id='tagName' value={tagName} onChange={e => setTagName(e.target.value)} />
-                                <br />
-                                <label htmlFor='tagColor'>Tag Color:</label>
-                                <SketchPicker color={tagColor} onChange={colorPickerHandler} />
-                                <div className='fm-container'>
-                                    <div className='fm-container-child'>
-                                        <button className='fm-save-button' onClick={saveTag}>save</button>
-                                    </div>
-                                    <div className='fm-container-child'>
-                                        <button className="fm-cancel-button" onClick={e => setShowTagCreateForm(false)}>cancel</button>
-                                    </div>
-                                </div>
+  useEffect(() => {
+    let options = [];
+    if (!tagOptionsState.loaded) {
+      fetch(getTagsUrl, {
+        method: "GET",
+        headers: { "Authorization": `Bearer ${token}` }
+      }).then(res => res.json())
+        .then(json => {
+          options = json.map(m => ({
+            label: m.TagName,
+            value: `${m.JobTagId}-${m.TagColour}`
+          }));
+          dispatch(actions.setTagOptions({ loaded: true, options }));
+        });
+    }
+  }, []);
 
-                            </div>
-                        </Swipeable>
+  useEffect(() => {
+    setAndCheckCreatableDropDown(tagOptionsState.options);
+  }, [tagOptionsState.options]);
+
+
+  const saveTag = () => {
+    let body = { TagName: tagName, TagColour: tagColor, JobId: 0, JobTagId: 0 };
+    fetch(getTagsUrl, {
+      method: "POST",
+      headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json;charset=UTF-8" },
+      body: JSON.stringify(body)
+    }).then(async res => {
+      if (res.status === 200) {
+        let id = await res.json();
+
+        let newTagOptionState = {
+          loaded: tagOptionsState.loaded, options: [...tagOptionsState.options, {
+            label: tagName,
+            value: `${id}-${tagColor}`
+          }]
+        };
+
+        dispatch(actions.setTagOptions(newTagOptionState));
+      }
+    }).then(r => setShowTagCreateForm(false));
+  };
+
+  return (
+    showElement && (
+      <div className="custom-select" style={{ width: '200px' }}>
+        {
+          !showTagCreateForm ? <Select
+            onChange={(option, { action }) => {
+              if (creatable) {
+                if (action === 'select-option') {
+                  if (option.value === 'create-tag') {
+                    setShowTagCreateForm(true);
+                    return;
+                  }
+                  if (option.value === 'no-tag') {
+                    dispatch(actions.setDefaultTag({}));
+                    return;
+                  }
+                  let toolNames = ['AnnotationCreateTextHighlight',
+                    'AnnotationCreateEllipse',
+                    'AnnotationCreateRectangle',
+                    'AnnotationCreateLine',
+                    'AnnotationCreateFreeHand',
+                    'AnnotationCreateFreeHandHighlight',
+                    'AnnotationCreatePolygon',
+                    'AnnotationCreatePolygonCloud',
+                    'AnnotationCreatePolyline',
+                    'AnnotationCreateArrow',
+                    'AnnotationCreateFreeText',
+                    'AnnotationCreateTextUnderline',
+                    'AnnotationCreateTextStrikeout',
+                    'AnnotationCreateTextSquiggly',
+                    'AnnotationCreateSticky'];
+                  let color = new window.Annotations.Color(option.value.split('-')[1]);
+                  toolNames.forEach(toolName => {
+                    if (toolName === 'AnnotationCreateFreeText') {
+                      setToolStyles(toolName, 'TextColor', color);
+                    } else {
+                      setToolStyles(toolName, 'StrokeColor', color);
+                    }
+                  });
+
+                  dispatch(actions.setDefaultTag(option));
                 }
-            </div>
-        )
+              } else {
+                if (setDropDownChanged) {
+                  setDropDownChanged(true);
+                }
+                if (setSelectedTags) {
+                  setSelectedTags(option);
+                }
+              }
+            }
+            }
+            ref={dropDownRef}
+            placeholder={placeholder}
+            defaultValue={creatable ? noTagOption : (defaultTag && defaultTag.value ? defaultTag : selectedTags)}
+            isSearchable
+            isClearable={creatable ? false : true}
+            options={tagOptions}
+            styles={customStyles}
+            closeMenuOnSelect={creatable ? true : false}
+            isMulti={creatable ? false : true}
+            noOptionsMessage={e => "no tags available"}
+          /> :
+            <Swipeable onSwipedUp={e => setShowTagCreateForm(false)} onSwipedDown={e => setShowTagCreateForm(false)} preventDefaultTouchmoveEvent>
+              <div style={{ marginTop: '380px' }}>
+
+                <label htmlFor="tagName">Tag Name:</label>
+                <input type="text" id="tagName" value={tagName} onChange={e => setTagName(e.target.value)} />
+                <br />
+                <label htmlFor="tagColor">Tag Color:</label>
+                <SketchPicker color={tagColor} onChange={colorPickerHandler} />
+                <div className="fm-container">
+                  <div className="fm-container-child">
+                    <button className="fm-save-button" onClick={saveTag}>save</button>
+                  </div>
+                  <div className="fm-container-child">
+                    <button className="fm-cancel-button" onClick={e => setShowTagCreateForm(false)}>cancel</button>
+                  </div>
+                </div>
+
+              </div>
+            </Swipeable>
+        }
+      </div>
     )
-})
+  );
+});
